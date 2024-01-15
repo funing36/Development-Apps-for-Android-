ca4ad613a69e371024d443de9ea51eef142cd518
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 15:27:44 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
 rm -rf Development-Apps-for-Android-
-claer
+clear
 echo "Setup Complete!"
