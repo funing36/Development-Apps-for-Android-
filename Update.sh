9b1b74b524c0a3da5d6dd9b7b962d040c56e55ee
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 15:34:41 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -3,5 +3,7 @@ git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
 rm -rf Development-Apps-for-Android-
+chmod +x Update.sh
+chmod +x Installs/VSCode_Setup.sh
 clear
 echo "Your software Update is complete!"
