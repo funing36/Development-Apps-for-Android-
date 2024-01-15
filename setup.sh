d3f79b6cdb95e7b67c533066d1e9f6a6b21f2156
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 15:24:22 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,7 @@ mkdir Software
 cd Software
 mkdir Software
 git clone https://github.com/funing36/Development-Apps-for-Android-.git
-rm README.md
-chmod +x Update.sh
-rm setup.sh
+mv installs 
+
 claer
 echo "Setup Complete!"
