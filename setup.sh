971730ccbeda0cbdac87a0239acdbaf166bdfd33
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 15:41:22 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,5 @@ git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
 rm -rf Development-Apps-for-Android-
-chmod +x Update.sh
-chmod +x Installs/VSCode_Setup.sh
 clear
-echo "Setup Complete!"
+echo "Setup Complete! Please run 'chmod +x Update.sh' to update in the future."
