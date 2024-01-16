8e7ec81f850547ef657356a24b76bc8126420d39
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 19:24:34 -0600

    Create Python3_pip.sh

---
+++ Installs/Python3_pip.sh
@@ -0,0 +1,11 @@
+sudo apt update
+clear
+sudo apt install python3
+clear
+sudo apt install python3-pip
+clear
+echo "Python and pip has been installed!"
+echo "Python Version:"
+python3 -v
+echo"PIP Version:"
+pip -v
