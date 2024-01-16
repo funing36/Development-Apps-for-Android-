6618f005161d7f86197004ba0eab30bf7b2dd39c
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 19:16:48 -0600

    Create nodejs_npm_setup.sh

---
+++ Installs/nodejs_np_setup.sh
@@ -0,0 +1,10 @@
+mkdir ../Software/nodejs
+cd ../Software/nodejs
+wget https://nodejs.org/dist/v18.12.0/node-v18.12.0-linux-arm64.tar.gz
+tar -xzf node-v18.12.0-linux-arm64.tar.gz
+sudo mv node-v18.12.0-linux-arm64 /usr/local/node-v18.12.0
+export PATH=$PATH:/usr/local/node-v18.12.0/bin
+clear
+echo "The instillation of NodeJS is compleat, running versions. If the version popes up, it's installed! If it errors out, something went wrong."
+node -v
+npm -v
