70c0cab58f4b27d5f82ece89bc60ec00a4b2046d
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 19:59:15 -0600

    Update VSCode_Setup.sh

--- Installs/VSCode_Setup.sh
+++ Installs/VSCode_Setup.sh
@@ -3,4 +3,5 @@ cd ../Software/VSCode
 echo -e "apt update -y\napt upgrade -y\nwget https://github.com/coder/code-server/releases/download/v4.16.1/code-server-4.16.1-linux-arm64.tar.gz\ntar -xvf code-server-4.16.1-linux-arm64.tar.gz\nrm code-server-4.16.1-linux-arm64.tar.gz\ncd code-server-4.16.1-linux-arm64\ncd bin\nexport PASSWORD=\"Password\"\nclear\necho \"Go to your browser and enter: localhost:8080\"\n./code-server" >> VSCode_Install.sh
 echo -e "apt update -y\napt upgrade -y\ncd code-server-4.16.1-linux-arm64/bin\nclear\necho \"Go to your browser and enter: localhost:8080\"\n./code-server" >> VSCode_launch.sh
 clear
-echo "All install files have been placed! Please go to 'cd ../Software/VSCode' and run 'chmod +x VSCode_Install.sh VSCode_launch.sh' then run './VSCode_install.sh'"
+sudo chmod +x VSCode_Install.sh VSCode_launch.sh
+echo "All install files have been placed! Please go to 'cd ../Software/VSCode' then run './VSCode_install.sh'"
