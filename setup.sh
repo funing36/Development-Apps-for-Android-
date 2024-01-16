c48f7a8dfe3a8d6a3655f7323eb0998c801be2d6
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-16 09:18:33 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ cd Software
 mkdir Software
 git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
+mv ./Development-Apps-for-Android-/Uninstall ./Uninstall
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
 rm -rf Development-Apps-for-Android-
 clear
