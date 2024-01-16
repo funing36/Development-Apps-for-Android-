780f225528120501bb797c7c23913b20ecb6fb91
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 19:54:57 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -7,4 +7,4 @@ rm -rf Development-Apps-for-Android-
 clear
 echo "Your software Update is complete! Were asking for sudo because of the chmod_everything script. You can tell what it does by the title."
 sudo chmod +x chmod_everything.sh
-./chmod_everything
+./chmod_everything.sh
