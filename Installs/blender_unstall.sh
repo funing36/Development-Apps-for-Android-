25c98bdd4e45c3296c259b803fa67c0ee5abdc82
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-16 09:14:21 -0600

    Create blender_unstall.sh

---
+++ Installs/blender_unstall.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install blender
+clear 
+echo "Blender has been installed! Warning, if you using an Android, this'll not be a pleasant unless you have a buffed up phone. Youll.need to start a VNC server by putting 'vncserver' to start one. When thats done, open a terminal in the VNC server and type in 'blender' to start it."
