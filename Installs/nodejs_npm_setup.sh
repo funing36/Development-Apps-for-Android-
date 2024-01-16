e747a2279e43d41a1553507a2ee53224fb741da1
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 19:25:59 -0600

    Update nodejs_npm_setup.sh

--- Installs/nodejs_npm_setup.sh
+++ Installs/nodejs_npm_setup.sh
@@ -6,5 +6,7 @@ sudo mv node-v18.12.0-linux-arm64 /usr/local/node-v18.12.0
 export PATH=$PATH:/usr/local/node-v18.12.0/bin
 clear
 echo "The instillation of NodeJS is compleat, running versions. If the version popes up, it's installed! If it errors out, something went wrong."
+echo "Node Version:"
 node -v
+echo "NPM version:"
 npm -v
