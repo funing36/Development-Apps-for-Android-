5a568de4283ad094665b3da233f785e718f4190c
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-15 19:46:58 -0600

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,6 +2,7 @@ rm -rf Installs
 git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
+mv ./Development-Apps-for-Android-/chmod_everything.sh ./chmod_everything.sh
 rm -rf Development-Apps-for-Android-
 clear
 echo "Your software Update is complete!"
