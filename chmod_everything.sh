42959074a71324ed5b0d54fba7ef0d41e5e85fa0
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-23 16:01:39 -0700

    Update chmod_everything.sh

--- chmod_everything.sh
+++ chmod_everything.sh
@@ -1,2 +1,2 @@
-sudo chmod +x Update.sh Installs/nodejs_npm_setup.sh Installs/Python3_pip.sh Installs/VSCode_Setup.sh Installs/blender_install.sh Uninstall/PIP-Python3-uninstall.sh Uninstall/Blender_uninstall.sh Uninstall/VSCode-uninstall.sh
+sudo chmod +x Update.sh Installs/nodejs_npm_setup.sh Installs/Python3_pip.sh Installs/VSCode_Setup.sh Installs/blender_install.sh Uninstall/PIP-Python3-uninstall.sh Uninstall/Blender_uninstall.sh Uninstall/VSCode-uninstall.sh Uninstall/NPM-Node-uninstall.sh
 echo "If everything worked correctly, everything should execute smoothly."
