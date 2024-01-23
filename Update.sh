2762151c056a1eabb7cfb0ddbcea8c6fe02fde5b
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-23 15:51:46 -0700

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -3,7 +3,7 @@ git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
 mv ./Development-Apps-for-Android-/chmod_everything.sh ./chmod_everything.sh
-mv ./Development-Apps-for-Android-/Uninstall./Uninstall 
+mv ./Development-Apps-for-Android-/Uninstall ./Uninstall 
 rm -rf Development-Apps-for-Android-
 clear
 echo "Your software Update is complete! Were asking for sudo because of the chmod_everything script. You can tell what it does by the title."
