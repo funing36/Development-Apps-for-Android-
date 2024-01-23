382f4dc1f2fc3c1ebfec8238d9c086a3b782870d
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-23 15:29:21 -0700

    Update VSCode_Setup.sh

--- Installs/VSCode_Setup.sh
+++ Installs/VSCode_Setup.sh
@@ -1,7 +1,7 @@
 mkdir ../Software/VSCode
 cd ../Software/VSCode
-echo -e "sudo apt update -y\napt upgrade -y\nwget https://github.com/coder/code-server/releases/download/v4.16.1/code-server-4.16.1-linux-arm64.tar.gz\ntar -xvf code-server-4.16.1-linux-arm64.tar.gz\nrm code-server-4.16.1-linux-arm64.tar.gz\ncd code-server-4.16.1-linux-arm64\ncd bin\nexport PASSWORD=\"Password\"\nclear\necho \"Go to your browser and enter: localhost:8080\"\n./code-server" >> VSCode_Install.sh
-echo -e "sudo apt update -y\napt upgrade -y\ncd code-server-4.16.1-linux-arm64/bin\nclear\necho \"Go to your browser and enter: localhost:8080\"\n./code-server" >> VSCode_launch.sh
+echo -e "sudo apt update -y\nsudo apt upgrade -y\nwget https://github.com/coder/code-server/releases/download/v4.16.1/code-server-4.16.1-linux-arm64.tar.gz\ntar -xvf code-server-4.16.1-linux-arm64.tar.gz\nrm code-server-4.16.1-linux-arm64.tar.gz\ncd code-server-4.16.1-linux-arm64\ncd bin\nexport PASSWORD=\"Password\"\nclear\necho \"Go to your browser and enter: localhost:8080\"\n./code-server" >> VSCode_Install.sh
+echo -e "sudo apt update -y\nsudo apt upgrade -y\ncd code-server-4.16.1-linux-arm64/bin\nclear\necho \"Go to your browser and enter: localhost:8080\"\n./code-server" >> VSCode_launch.sh
 clear
 sudo chmod +x VSCode_Install.sh VSCode_launch.sh
 echo "All install files have been placed! Please go to 'cd ../Software/VSCode' then run './VSCode_install.sh'"
