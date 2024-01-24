6f32c8ba62ccafa213bce361a10796620d903eb6
PebbleCat <146106615+funing36@users.noreply.github.com>
2024-01-23 22:44:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@ cd Software
 mkdir Software
 git clone https://github.com/funing36/Development-Apps-for-Android-.git
 mv ./Development-Apps-for-Android-/Installs ./Installs
-mv ./Development-Apps-for-Android-/Uninstall ./Uninstall
 mv ./Development-Apps-for-Android-/Update.sh ./Update.sh
+mv ./Development-Apps-for-Android-/Uninstall ./Uninstall 
+mv ./Development-Apps-for-Android-/chmod_everything.sh ./chmod_everything.sh
 rm -rf Development-Apps-for-Android-
 clear
 echo "Setup Complete! Please run 'chmod +x Update.sh' to update in the future."
